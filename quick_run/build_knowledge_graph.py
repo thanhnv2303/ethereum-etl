cc78986f9e413565da9c950f09c9f654c441ff76
thanhnv <nguyenthanh2303@gmail.com>
2021-05-26 01:23:12 +0700

    fix: add join parth

--- quick_run/build_knowledge_graph.py
+++ quick_run/build_knowledge_graph.py
@@ -15,56 +15,58 @@ from ethereumetl.providers.auto import get_provider_from_uri
 from ethereumetl.streaming.eth_knowledge_graph_streamer_adapter import EthKnowledgeGraphStreamerAdapter
 from ethereumetl.thread_local_proxy import ThreadLocalProxy
 
-last_synced_block_file = "./data/last_synced_block.txt"
-lag = 0
-# log_file = "./data_stream/logs.txt"
-entity_types = ','.join(EntityType.ALL_TOKEN)
-output = "knowledge_graph"
-geth_ipc_file = "~/bsc-full-sync/node/geth.ipc"
-if not path.exists(geth_ipc_file):
-    provider_uri = "http://25.19.185.225:8545"
-else:
-    provider_uri = "file:///" + geth_ipc_file
-batch_size = 128
-max_workers = 8
+if __name__ == '__main__':
 
-# start_block = 4678378
-if path.exists(last_synced_block_file):
-    start_block = None
-else:
-    start_block = 2472670
-period_seconds = 10
-pid_file = None
-block_batch_size = 16
+    last_synced_block_file = "./data/last_synced_block.txt"
+    lag = 0
+    # log_file = "./data_stream/logs.txt"
+    entity_types = ','.join(EntityType.ALL_TOKEN)
+    output = "knowledge_graph"
+    geth_ipc_file = "~/bsc-full-sync/node/geth.ipc"
+    if not path.exists(geth_ipc_file):
+        provider_uri = "http://25.19.185.225:8545"
+    else:
+        provider_uri = "file:///" + geth_ipc_file
+    batch_size = 128
+    max_workers = 8
 
-# configure_logging(log_file)
-configure_signals()
-entity_types = parse_entity_types(entity_types)
-validate_entity_types(entity_types, output)
+    # start_block = 4678378
+    if path.exists(last_synced_block_file):
+        start_block = None
+    else:
+        start_block = 2472670
+    period_seconds = 10
+    pid_file = None
+    block_batch_size = 16
 
-from ethereumetl.streaming.item_exporter_creator import create_item_exporter
-from blockchainetl.streaming.streamer import Streamer
+    # configure_logging(log_file)
+    configure_signals()
+    entity_types = parse_entity_types(entity_types)
+    validate_entity_types(entity_types, output)
 
-# TODO: Implement fallback mechanism for provider uris instead of picking randomly
-provider_uri = pick_random_provider_uri(provider_uri)
-logging.info('Using ' + provider_uri)
+    from ethereumetl.streaming.item_exporter_creator import create_item_exporter
+    from blockchainetl.streaming.streamer import Streamer
 
-streamer_adapter = EthKnowledgeGraphStreamerAdapter(
-    provider_uri=provider_uri,
-    batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
-    item_exporter=create_item_exporter(output),
-    token_filter=VENUS_TOKEN,
-    batch_size=batch_size,
-    max_workers=max_workers,
-    entity_types=entity_types
-)
-streamer = Streamer(
-    blockchain_streamer_adapter=streamer_adapter,
-    last_synced_block_file=last_synced_block_file,
-    lag=lag,
-    start_block=start_block,
-    period_seconds=period_seconds,
-    block_batch_size=block_batch_size,
-    pid_file=pid_file
-)
-streamer.stream()
+    # TODO: Implement fallback mechanism for provider uris instead of picking randomly
+    provider_uri = pick_random_provider_uri(provider_uri)
+    logging.info('Using ' + provider_uri)
+
+    streamer_adapter = EthKnowledgeGraphStreamerAdapter(
+        provider_uri=provider_uri,
+        batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
+        item_exporter=create_item_exporter(output),
+        token_filter=VENUS_TOKEN,
+        batch_size=batch_size,
+        max_workers=max_workers,
+        entity_types=entity_types
+    )
+    streamer = Streamer(
+        blockchain_streamer_adapter=streamer_adapter,
+        last_synced_block_file=last_synced_block_file,
+        lag=lag,
+        start_block=start_block,
+        period_seconds=period_seconds,
+        block_batch_size=block_batch_size,
+        pid_file=pid_file
+    )
+    streamer.stream()
